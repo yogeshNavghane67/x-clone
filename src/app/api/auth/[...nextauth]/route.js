020d0bba997407be7de3cd2adb6064ecd638620a
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-07 21:52:26 +0530

    mini profile to sidebar component

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,21 @@
 import NextAuth from "next-auth/next";
 
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ]
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks:{
+    async session({ session, token }) {
+        session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase();
+        session.user.uid = token.sub;
+        return session;
+    }
+  }
+});
 
-export { handler as GET, handler as POST};
+export { handler as GET, handler as POST };
