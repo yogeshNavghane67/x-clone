9be07f9ca57066fe6d9414538c761c786869aa77
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-07 21:23:59 +0530

    add next-auth and modify the sidebar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import News from "@/components/News";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <SessionWrapper>
     <html lang="en">
       <body className={inter.className}>
         <div className="flex justify-between max-w-6xl mx-auto">
@@ -28,5 +30,6 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
+    </SessionWrapper>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,33 @@
+'use client';
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
+  const { data: session } = useSession(); 
   return (
     <div className="flex flex-col gap-4 p-3">
-      <Link href='/'>
-      <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200"/>
+      <Link href="/">
+        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
       </Link>
-      <Link href='/' className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit">
-      <HiHome className="w-16 h-16 "/>
-      <span className="font-bold hidden xl:inline">Home</span>
+      <Link
+        href="/"
+        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
+      >
+        <HiHome className="w-16 h-16 " />
+        <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold">Sign In</button>
+      {session ? (
+        <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold" onClick={() => signOut()}>
+        Sign Out
+      </button>
+      ) : (
+        <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold" onClick={() => signIn()}>
+        Sign In
+      </button>
+      )}
     </div>
-  )
+  );
 }
